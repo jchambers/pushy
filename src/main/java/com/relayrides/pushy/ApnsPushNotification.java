4e78247c2c1be675a4ac18557665f9e97c62d3ec
Jon Chambers <jon.chambers@gmail.com>
2013-08-04 11:40:00 -0400

    Added some docs around the ApnsPushNotification interface.

--- src/main/java/com/relayrides/pushy/ApnsPushNotification.java
+++ src/main/java/com/relayrides/pushy/ApnsPushNotification.java
@@ -3,7 +3,34 @@ package com.relayrides.pushy;
 import java.util.Date;
 
 public interface ApnsPushNotification {
-	byte[] getTokenBytes();
-	byte[] getPayloadBytes();
+	/**
+	 * Returns the token of the device to which this push notification is to be sent.
+	 * 
+	 * @return an array of bytes containing the token of the device to which this notification is to be sent
+	 * 
+	 * @see <a href="https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW9">
+	 * Apple Push Notification Service</a>
+	 */
+	byte[] getToken();
+	
+	/**
+	 * Returns the JSON-encoded payload of this push notification.
+	 * 
+	 * @return the JSON-encoded payload of this push notification
+	 * 
+	 * @see <a href="https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW9">
+	 * Apple Push Notification Service</a>
+	 */
+	String getPayload();
+	
+	/**
+	 * Returns the time at which Apple's push notification service should stop trying to deliver this push notification
+	 * to its destination. If null, the push notification service will not attempt to store the notification at all.
+	 * 
+	 * @return the time at which this notification can be discarded
+	 * 
+	 * @see <a href="https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/CommunicatingWIthAPS.html#//apple_ref/doc/uid/TP40008194-CH101-SW1">
+	 * Provider Communication with Apple Push Notification Service</a>
+	 */
 	Date getExpiration();
 }
--- src/main/java/com/relayrides/pushy/apns/PushNotificationEncoder.java
+++ src/main/java/com/relayrides/pushy/apns/PushNotificationEncoder.java
@@ -4,21 +4,34 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
+import java.nio.charset.Charset;
 import java.util.Date;
 
 public class PushNotificationEncoder extends MessageToByteEncoder<SendableApnsPushNotification> {
 
-	final byte PUSH_NOTIFICATION_COMMAND = 1;
+	private static final byte ENHANCED_PUSH_NOTIFICATION_COMMAND = 1;
+	private static final int EXPIRE_IMMEDIATELY = 0;
+	
+	private final Charset utf8 = Charset.forName("UTF-8");
 	
 	@Override
 	protected void encode(final ChannelHandlerContext context, final SendableApnsPushNotification sendablePushNotification, final ByteBuf out) throws Exception {
-		out.writeByte(PUSH_NOTIFICATION_COMMAND);
+		out.writeByte(ENHANCED_PUSH_NOTIFICATION_COMMAND);
 		out.writeInt(sendablePushNotification.getNotificationId());
-		out.writeInt(this.getTimestampInSeconds(sendablePushNotification.getPushNotification().getExpiration()));
-		out.writeShort(sendablePushNotification.getPushNotification().getTokenBytes().length);
-		out.writeBytes(sendablePushNotification.getPushNotification().getTokenBytes());
-		out.writeShort(sendablePushNotification.getPushNotification().getPayloadBytes().length);
-		out.writeBytes(sendablePushNotification.getPushNotification().getPayloadBytes());
+		
+		if (sendablePushNotification.getPushNotification().getExpiration() != null) {
+			out.writeInt(this.getTimestampInSeconds(sendablePushNotification.getPushNotification().getExpiration()));
+		} else {
+			out.writeInt(EXPIRE_IMMEDIATELY);
+		}
+		
+		out.writeShort(sendablePushNotification.getPushNotification().getToken().length);
+		out.writeBytes(sendablePushNotification.getPushNotification().getToken());
+		
+		final byte[] payloadBytes = sendablePushNotification.getPushNotification().getPayload().getBytes(utf8);
+		
+		out.writeShort(payloadBytes.length);
+		out.writeBytes(payloadBytes);
 	}
 	
 	private int getTimestampInSeconds(final Date date) {
