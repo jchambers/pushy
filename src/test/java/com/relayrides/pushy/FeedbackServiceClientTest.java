209ecfcbd5429976d84c5790b38b15ca0e92529b
Jon Chambers <jon@relayrides.com>
2013-08-06 12:08:37 -0400

    Added netty as a proper maven dependency.

--- src/test/java/com/relayrides/pushy/FeedbackServiceClientTest.java
+++ src/test/java/com/relayrides/pushy/FeedbackServiceClientTest.java
@@ -1,6 +1,7 @@
 package com.relayrides.pushy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 import java.util.List;
