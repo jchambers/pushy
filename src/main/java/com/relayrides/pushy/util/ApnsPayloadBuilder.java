e6e803d6cbfedb1be3e45bb42dd4f9cfec42b828
Jon Chambers <jon@relayrides.com>
2013-08-07 19:10:39 -0400

    Added a payload builder and supporting tests. Added json.simple as a dependency to support the payload builder.

---
+++ src/main/java/com/relayrides/pushy/util/ApnsPayloadBuilder.java
@@ -0,0 +1,358 @@
+package com.relayrides.pushy.util;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class ApnsPayloadBuilder {
+	
+	private String alertBody = null;
+	private String localizedAlertKey = null;
+	private String[] localizedAlertArguments = null;
+	private String launchImageFileName = null;
+	private boolean showActionButton = true;
+	private String localizedActionButtonKey = null;
+	private Integer badgeNumber = null;
+	private String soundFileName = null;
+	
+	private static final String APS_KEY = "aps";
+	private static final String ALERT_KEY = "alert";
+	private static final String BADGE_KEY = "badge";
+	private static final String SOUND_KEY = "sound";
+	
+	private static final String ALERT_BODY_KEY = "body";
+	private static final String ACTION_LOC_KEY = "action-loc-key";
+	private static final String ALERT_LOC_KEY = "loc-key";
+	private static final String ALERT_ARGS_KEY = "loc-args";
+	private static final String LAUNCH_IMAGE_KEY = "launch-image";
+	
+	private final HashMap<String, Object> customProperties = new HashMap<String, Object>();
+	
+	private static final int DEFAULT_PAYLOAD_SIZE = 256;
+	
+	/**
+	 * <p>Sets the literal text of the alert message to be shown for the push notification. A literal alert message may
+	 * not be set if a localized alert message key is already specified.</p>
+	 * 
+	 * <p>By default, no message is shown.</p>
+	 * 
+	 * @param alertBody the literal message to be shown for this push notification
+	 * 
+	 * @see ApnsPayloadBuilder#setLocalizedAlertMessage(String, String[])
+	 */
+	public void setAlertBody(final String alertBody) {
+		if (alertBody != null && this.localizedAlertKey != null) {
+			throw new IllegalStateException("Cannot set a literal alert body when a localized alert key has already been set.");
+		}
+		
+		this.alertBody = alertBody;
+	}
+	
+	/**
+	 * <p>Sets the key of a message in the receiving app's localized string list to be shown for the push notification.
+	 * The message in the app's string list may optionally have placeholders, which will be populated by values from the
+	 * given {@code alertArguments}.</p>
+	 * 
+	 * <p>By default, no message is shown.</p>
+	 * 
+	 * @param localizedAlertKey a key to a string in the receiving app's localized string list
+	 * @param alertArguments arguments to populate placeholders in the localized alert string; may be {@code null}
+	 */
+	public void setLocalizedAlertMessage(final String localizedAlertKey, final String[] alertArguments) {
+		if (localizedAlertKey != null && this.alertBody != null) {
+			throw new IllegalStateException("Cannot set a localized alert key when a literal alert body has already been set.");
+		}
+		
+		if (localizedAlertKey == null && alertArguments != null) {
+			throw new IllegalArgumentException("Cannot set localized alert arguments without a localized alert message key.");
+		}
+		
+		this.localizedAlertKey = localizedAlertKey;
+		this.localizedAlertArguments = alertArguments;
+	}
+	
+	/**
+	 * <p>Sets the image to be shown when the receiving app launches in response to this push notification. According
+	 * to Apple's documentation, this should be:</p>
+	 * 
+	 * <blockquote>The filename of an image file in the application bundle; it may include the extension or omit it.
+	 * The image is used as the launch image when users tap the action button or move the action slider. If this
+	 * property is not specified, the system either uses the previous snapshot, uses the image identified by the
+	 * @{code UILaunchImageFile} key in the application’s {@code Info.plist} file, or falls back to
+	 * {@code Default.png}.</blockquote>
+	 * 
+	 * @param launchImageFilename the filename of an image file in the receiving app's bundle to be shown when launching
+	 * the app from the push notification
+	 */
+	public void setLaunchImage(final String launchImageFilename) {
+		this.launchImageFileName = launchImageFilename;
+	}
+	
+	/**
+	 * <p>Sets whether an &quot;action&quot; button should be shown if the push notification is displayed as an alert.
+	 * If {@code true} and no localized action button key is set, the default label (defined by the receiving operating
+	 * system) is used. If @{code true} and a localized action button key is set, the string for that key is used as
+	 * the label of the action button. If {@code false}, no action button is shown under any circumstances</p>
+	 * 
+	 * <p>By default, an action button will be shown.</p>
+	 * 
+	 * @param showActionButton {@code true} to show an action button when the push notification is presented as an
+	 * alert or {@code false} to show an alert with no action button
+	 */
+	public void setShowActionButton(final boolean showActionButton) {
+		this.showActionButton = showActionButton;
+	}
+	
+	/**
+	 * <p>Sets the key of a string in the receiving app's localized string list to be used as the label of the
+	 * &quot;action&quot; button if the push notification is displayed as an alert. By default, the OS-default label
+	 * will be used for the action button.</p>
+	 * 
+	 * @param localizedActionButtonKey a key to a string in the receiving app's localized string list
+	 */
+	public void setLocalizedActionButtonKey(final String localizedActionButtonKey) {
+		this.localizedActionButtonKey = localizedActionButtonKey;
+	}
+	
+	/**
+	 * <p>Sets the number to display as the badge of the icon of the application that receives the push notification.
+	 * If the badge number is 0, the badge is removed from the application icon. If {@code null}, the badge is left in
+	 * its current state. By default, no change is made to the badge.</p>
+	 * 
+	 * @param badge the number to display as the badge of application or {@code null} to leave the badge unchanged
+	 */
+	public void setBadgeNumber(final Integer badgeNumber) {
+		this.badgeNumber = badgeNumber;
+	}
+	
+	/**
+	 * <p>Sets the name of the sound file to play when the push notification is received. According to Apple's
+	 * documentation, the value here should be:</p>
+	 * 
+	 * <blockquote>...the name of a sound file in the application bundle. The sound in this file is played as an alert.
+	 * If the sound file doesn't exist or default is specified as the value, the default alert sound is
+	 * played.</blockquote>
+	 * 
+	 * <p>By default, no sound is included in the push notification.</p>
+	 * 
+	 * @param soundFileName the name of the sound file to play, or {@code null} to send no sound
+	 */
+	public void setSoundFileName(final String soundFileName) {
+		this.soundFileName = soundFileName;
+	}
+	
+	/**
+	 * <p>Adds a custom property to the payload. Values are mapped to JSON types according to the mapping table at
+	 * <a href="https://code.google.com/p/json-simple/">https://code.google.com/p/json-simple/</a>.</p>
+	 * 
+	 * <p>According to Apple's documentation:</p>
+	 * 
+	 * <blockquote>Providers can specify custom payload values outside the Apple-reserved {@code aps} namespace. Custom
+	 * values must use the JSON structured and primitive types: dictionary (object), array, string, number, and Boolean.
+	 * You should not include customer information (or any sensitive data) as custom payload data. Instead, use it for
+	 * such purposes as setting context (for the user interface) or internal metrics. For example, a custom payload
+	 * value might be a conversation identifier for use by an instant-message client application or a timestamp
+	 * identifying when the provider sent the notification. Any action associated with an alert message should not be
+	 * destructive—for example, it should not delete data on the device.</blockquote>
+	 * 
+	 * @param key the key of the custom property in the payload object
+	 * @param value the value of the custom property
+	 */
+	public void addCustomProperty(final String key, final Object value) {
+		this.customProperties.put(key, value);
+	}
+	
+	/**
+	 * <p>Returns a JSON representation of the push notification payload under construction. If the payload length is
+	 * longer than the default maximum, the literal alert body will be shortened if possible. If the alert body cannot
+	 * be shortened or is not present, an {@code IllegalArgumentException} is thrown.</p>
+	 * 
+	 * @return a JSON representation of the payload under construction (possibly with an abbreviated alert body)
+	 */
+	public String buildWithDefaultMaximumLength() {
+		return this.buildWithMaximumLength(DEFAULT_PAYLOAD_SIZE);
+	}
+	
+	/**
+	 * <p>Returns a JSON representation of the push notification payload under construction. If the payload length is
+	 * longer than the given maximum, the literal alert body will be shortened if possible. If the alert body cannot be
+	 * shortened or is not present, an {@code IllegalArgumentException} is thrown.</p>
+	 * 
+	 * @param maximumPayloadLength the maximum length of the payload in bytes
+	 * 
+	 * @return a JSON representation of the payload under construction (possibly with an abbreviated alert body)
+	 */
+	@SuppressWarnings("unchecked")
+	public String buildWithMaximumLength(final int maximumPayloadLength) {
+		final JSONObject payload = new JSONObject();
+		
+		{
+			final JSONObject aps = new JSONObject();
+			
+			if (this.badgeNumber != null) {
+				aps.put(BADGE_KEY, this.badgeNumber);
+			}
+			
+			if (this.soundFileName != null) {
+				aps.put(SOUND_KEY, this.soundFileName);
+			}
+			
+			final Object alertObject = this.createAlertObject();
+			
+			if (alertObject != null) {
+				aps.put(ALERT_KEY, alertObject);
+			}
+			
+			payload.put(APS_KEY, aps);
+		}
+		
+		for (final Map.Entry<String, Object> entry : this.customProperties.entrySet()) {
+			payload.put(entry.getKey(), entry.getValue());
+		}
+		
+		final String payloadString = payload.toJSONString();
+		final int initialPayloadLength = payloadString.getBytes(Charset.forName("UTF-8")).length;
+		
+		if (initialPayloadLength <= maximumPayloadLength) {
+			return payloadString;
+		} else {
+			// TODO This could probably be more efficient
+			if (this.alertBody != null) {
+				this.replaceMessageBody(payload, "");
+				final int payloadLengthWithEmptyMessage = payload.toJSONString().getBytes(Charset.forName("UTF-8")).length;
+				
+				if (payloadLengthWithEmptyMessage > maximumPayloadLength) {
+					throw new IllegalArgumentException("Payload exceeds maximum length even with an empty message body.");
+				}
+				
+				int maximumMessageBodyLength = maximumPayloadLength - payloadLengthWithEmptyMessage;
+				
+				this.replaceMessageBody(payload, this.abbreviateString(this.alertBody, maximumMessageBodyLength--));
+				
+				while (payload.toJSONString().getBytes(Charset.forName("UTF-8")).length > maximumPayloadLength) {
+					this.replaceMessageBody(payload, this.abbreviateString(this.alertBody, maximumMessageBodyLength--));
+				}
+				
+				return payload.toJSONString();
+				
+			} else {
+				throw new IllegalArgumentException(String.format(
+						"Payload length is %d bytes (with a maximum of %d bytes) and cannot be shortened.",
+						initialPayloadLength, maximumPayloadLength));
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void replaceMessageBody(final JSONObject payload, final String messageBody) {
+		final JSONObject aps = (JSONObject) payload.get(APS_KEY);
+		final Object alert = aps.get(ALERT_KEY);
+		
+		if (alert != null) {
+			if (alert instanceof String) {
+				aps.put(ALERT_KEY, messageBody);
+			} else {
+				final JSONObject alertObject = (JSONObject) alert;
+				
+				if (alertObject.get(ALERT_BODY_KEY) != null) {
+					alertObject.put(ALERT_BODY_KEY, messageBody);
+				} else {
+					throw new IllegalArgumentException("Payload has no message body.");
+				}
+			}
+		} else {
+			throw new IllegalArgumentException("Payload has no message body.");
+		}
+	}
+	
+	private final String abbreviateString(final String string, final int maximumLength) {
+		if (string.length() <= maximumLength) {
+			return string;
+		} else {
+			if (maximumLength <= 3) {
+				throw new IllegalArgumentException("Cannot abbreviate string to fewer than three characters.");
+			}
+			
+			// TODO Actually use a real ellipses character when https://code.google.com/p/json-simple/issues/detail?id=25 gets resolved
+			// TODO Make this locale-sensitive
+			return string.substring(0, maximumLength - 3) + "...";
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Object createAlertObject() {
+		if (this.hasAlertContent()) {
+			if (this.shouldRepresentAlertAsString()) {
+				return this.alertBody;
+			} else {
+				final JSONObject alert = new JSONObject();
+				
+				if (this.alertBody != null) {
+					alert.put(ALERT_BODY_KEY, this.alertBody);
+				}
+				
+				if (this.showActionButton) {
+					if (this.localizedActionButtonKey != null) {
+						alert.put(ACTION_LOC_KEY, this.localizedActionButtonKey);
+					}
+				} else {
+					// To hide the action button, the key needs to be present, but the value needs to be null
+					alert.put(ACTION_LOC_KEY, null);
+				}
+				
+				if (this.localizedAlertKey != null) {
+					alert.put(ALERT_LOC_KEY, this.localizedAlertKey);
+					
+					if (this.localizedAlertArguments != null) {
+						final JSONArray alertArgs = new JSONArray();
+						
+						for (final String arg : this.localizedAlertArguments) {
+							alertArgs.add(arg);
+						}
+						
+						alert.put(ALERT_ARGS_KEY, alertArgs);
+					}
+				}
+				
+				if (this.launchImageFileName != null) {
+					alert.put(LAUNCH_IMAGE_KEY, this.launchImageFileName);
+				}
+				
+				return alert;
+			}
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Checks whether the notification under construction has content that warrants an {@code alert} section.
+	 * 
+	 * @return {@code true} if this notification should have an {@code alert} section or {@code false} otherwise
+	 */
+	private boolean hasAlertContent() {
+		return	this.alertBody != null ||
+				this.localizedAlertKey != null ||
+				this.localizedActionButtonKey != null ||
+				this.launchImageFileName != null ||
+				this.showActionButton == false;
+	}
+	
+	/**
+	 * <p>Checks whether the alert message for the push notification should be represented as a string or a
+	 * dictionary. According to Apple's documentation:</p>
+	 * 
+	 * <blockquote>If you want the device to display the message text as-is in an alert that has both the Close and
+	 * View buttons, then specify a string as the direct value of {@code alert}. Don't specify a dictionary as the
+	 * value of {@code alert} if the dictionary only has the {@code body} property.</blockquote>
+	 * 
+	 * @return {@code true} if the alert message (if present) should be represented as a string or {@code false} if it
+	 * should be represented as a dictionary
+	 */
+	private boolean shouldRepresentAlertAsString() {
+		return this.alertBody != null && this.launchImageFileName == null && this.showActionButton && this.localizedActionButtonKey == null;
+	}
+}
---
+++ src/test/java/com/relayrides/pushy/util/ApnsPayloadBuilderTest.java
@@ -0,0 +1,222 @@
+package com.relayrides.pushy.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.charset.Charset;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApnsPayloadBuilderTest {
+
+	private ApnsPayloadBuilder builder;
+	private final JSONParser parser;
+	
+	public ApnsPayloadBuilderTest() {
+		this.parser = new JSONParser();
+	}
+	
+	@Before
+	public void setUp() {
+		this.builder = new ApnsPayloadBuilder();
+	}
+	
+	@Test
+	public void testSetAlertBody() throws ParseException {
+		final String alertBody = "This is a test alert message.";
+		
+		this.builder.setAlertBody(alertBody);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			
+			// The alert property should be a string if all we're specifying is a literal alert message
+			assertEquals(alertBody, aps.get("alert"));
+		}
+		
+		this.builder.setShowActionButton(false);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			final JSONObject alert = (JSONObject) aps.get("alert");
+			
+			assertEquals(alertBody, alert.get("body"));
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testSetLocalizedAlertMessage() throws ParseException {
+		final String alertKey = "test.alert";
+		this.builder.setLocalizedAlertMessage(alertKey, null);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			final JSONObject alert = (JSONObject) aps.get("alert");
+			
+			assertEquals(alertKey, alert.get("loc-key"));
+			assertNull(alert.get("loc-args"));
+		}
+		
+		final String[] alertArgs = new String[] { "Moose", "helicopter" };
+		this.builder.setLocalizedAlertMessage(alertKey, alertArgs);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			final JSONObject alert = (JSONObject) aps.get("alert");
+			
+			assertEquals(alertKey, alert.get("loc-key"));
+			
+			final JSONArray argsArray = (JSONArray) alert.get("loc-args");
+			assertEquals(alertArgs.length, argsArray.size());
+			assertTrue(argsArray.containsAll(java.util.Arrays.asList(alertArgs)));
+		}
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testSetAlertBodyWithExistingLocalizedAlert() {
+		this.builder.setLocalizedAlertMessage("Test", null);
+		this.builder.setAlertBody("Test");
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testSetLocalizedAlertWithExistingAlertBody() {
+		this.builder.setAlertBody("Test");
+		this.builder.setLocalizedAlertMessage("Test", null);
+	}
+
+	@Test
+	public void testSetLaunchImage() throws ParseException {
+		final String launchImageFilename = "launch.png";
+		this.builder.setLaunchImage(launchImageFilename);
+		
+		final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+		final JSONObject alert = (JSONObject) aps.get("alert");
+		
+		assertEquals(launchImageFilename, alert.get("launch-image"));
+	}
+
+	@Test
+	public void testSetShowActionButton() throws ParseException {
+		this.builder.setShowActionButton(true);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			
+			assertNull(aps.get("alert"));
+		}
+		
+		this.builder.setShowActionButton(false);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			final JSONObject alert = (JSONObject) aps.get("alert");
+			
+			assertTrue(alert.keySet().contains("action-loc-key"));
+			assertNull(alert.get("action-loc-key"));
+		}
+		
+		final String actionButtonKey = "action.key";
+		this.builder.setLocalizedActionButtonKey(actionButtonKey);
+		this.builder.setShowActionButton(true);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			final JSONObject alert = (JSONObject) aps.get("alert");
+			
+			assertEquals(actionButtonKey, alert.get("action-loc-key"));
+		}
+		
+		this.builder.setShowActionButton(false);
+		
+		{
+			final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+			final JSONObject alert = (JSONObject) aps.get("alert");
+			
+			assertTrue(alert.keySet().contains("action-loc-key"));
+			assertNull(alert.get("action-loc-key"));
+		}
+	}
+
+	@Test
+	public void testSetLocalizedActionButtonKey() throws ParseException {
+		final String actionButtonKey = "action.key";
+		this.builder.setLocalizedActionButtonKey(actionButtonKey);
+		
+		final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+		final JSONObject alert = (JSONObject) aps.get("alert");
+		
+		assertEquals(actionButtonKey, alert.get("action-loc-key"));
+	}
+
+	@Test
+	public void testSetBadgeNumber() throws ParseException {
+		final int badgeNumber = 4;
+		this.builder.setBadgeNumber(badgeNumber);
+		
+		final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+		
+		assertEquals(badgeNumber, ((Number) aps.get("badge")).intValue());
+	}
+
+	@Test
+	public void testSetSoundFileName() throws ParseException {
+		final String soundFileName = "dying-giraffe.aiff";
+		this.builder.setSoundFileName(soundFileName);
+		
+		final JSONObject aps = this.extractApsObjectFromPayloadString(this.builder.buildWithDefaultMaximumLength());
+		
+		assertEquals(soundFileName, aps.get("sound"));
+	}
+
+	@Test
+	public void testAddCustomProperty() throws ParseException {
+		final String customKey = "string";
+		final String customValue = "Hello";
+		
+		this.builder.addCustomProperty(customKey, customValue);
+		
+		final JSONObject payload = (JSONObject) this.parser.parse(this.builder.buildWithDefaultMaximumLength());
+		
+		assertEquals(customValue, payload.get(customKey));
+	}
+
+	@Test
+	public void testBuildWithMaximumLength() {
+		final String reallyLongAlertMessage =
+				"All non-glanded recruited mercenaries now engaging in training excercises are herefore and forever ordered to desist. Aforementioned activities have resulted in cost-defective damage to training areas.";
+		
+		final int maxLength = 128;
+		
+		this.builder.setAlertBody(reallyLongAlertMessage);
+		
+		final String payloadString = this.builder.buildWithMaximumLength(maxLength);
+		
+		assertTrue(payloadString.getBytes(Charset.forName("UTF-8")).length <= maxLength);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testBuildWithMaximumLengthAndUnshortenablePayload() {
+		final String reallyLongAlertKey =
+				"All non-glanded recruited mercenaries now engaging in training excercises are herefore and forever ordered to desist. Aforementioned activities have resulted in cost-defective damage to training areas.";
+		
+		final int maxLength = 128;
+		
+		this.builder.setLocalizedAlertMessage(reallyLongAlertKey, null);
+		
+		final String payloadString = this.builder.buildWithMaximumLength(maxLength);
+		
+		assertTrue(payloadString.getBytes(Charset.forName("UTF-8")).length <= maxLength);
+	}
+	
+	private JSONObject extractApsObjectFromPayloadString(final String payloadString) throws ParseException {
+		final JSONObject payload = (JSONObject) this.parser.parse(payloadString);
+		return (JSONObject) payload.get("aps");
+	}
+}
