50f868c582399dc1b5ce13f043f42e46e71b1dad
Jon Chambers <jon@relayrides.com>
2013-08-08 19:33:34 -0400

    Added class-level docs to the ApnsPushNotification interface.

--- src/main/java/com/relayrides/pushy/apns/ApnsPushNotification.java
+++ src/main/java/com/relayrides/pushy/apns/ApnsPushNotification.java
@@ -2,14 +2,27 @@ package com.relayrides.pushy.apns;
 
 import java.util.Date;
 
+import com.relayrides.pushy.util.ApnsPayloadBuilder;
+import com.relayrides.pushy.util.TokenUtil;
+
+/**
+ * <p>A push notification that can be sent through the Apple Push Notification service (APNs). Push notifications have
+ * a token that identifies the device to which it should be sent, a JSON payload, and (optionally) a time at which the
+ * notification is invalid and should no longer be delivered.</p>
+ *
+ * @author <a href="mailto:jon@relayrides.com">Jon Chambers</a>
+ *
+ * @see <a href="https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW9">
+ * Apple Push Notification Service</a>
+ * 
+ * @see TokenUtil
+ * @see ApnsPayloadBuilder
+ */
 public interface ApnsPushNotification {
 	/**
 	 * Returns the token of the device to which this push notification is to be sent.
 	 * 
 	 * @return an array of bytes containing the token of the device to which this notification is to be sent
-	 * 
-	 * @see <a href="https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW9">
-	 * Apple Push Notification Service</a>
 	 */
 	byte[] getToken();
 	
@@ -17,15 +30,13 @@ public interface ApnsPushNotification {
 	 * Returns the JSON-encoded payload of this push notification.
 	 * 
 	 * @return the JSON-encoded payload of this push notification
-	 * 
-	 * @see <a href="https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW9">
-	 * Apple Push Notification Service</a>
 	 */
 	String getPayload();
 	
 	/**
 	 * Returns the time at which Apple's push notification service should stop trying to deliver this push notification
-	 * to its destination. If null, the push notification service will not attempt to store the notification at all.
+	 * to its destination. If @{code null}, the push notification service will not attempt to store the notification at
+	 * all.
 	 * 
 	 * @return the time at which this notification can be discarded
 	 * 
