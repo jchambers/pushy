66f0fe847299c82ab5ba14fce181ebf76f1fd1dc
Jon Chambers <jon@relayrides.com>
2013-08-08 14:39:54 -0400

    Added docs for the ApnsEnvironment class.

--- src/main/java/com/relayrides/pushy/apns/ApnsEnvironment.java
+++ src/main/java/com/relayrides/pushy/apns/ApnsEnvironment.java
@@ -1,5 +1,16 @@
 package com.relayrides.pushy.apns;
 
+/**
+ * <p>An APNs environment is a set of servers that provide push notification services. Apple provides two environments:
+ * one production environment and one &quot;sandbox&quot; environment. Custom environments may be created for
+ * development and testing purposes.</p>
+ * 
+ * <p>APNs environments may optionally require TLS. Both Apple-provided environments require TLS. See Apple's
+ * <a href="http://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ProvisioningDevelopment.html#//apple_ref/doc/uid/TP40008194-CH104-SW1">
+ * &quot;Provisioning and Development&quot;</a> for details.</p>
+ *
+ * @author <a href="mailto:jon@relayrides.com">Jon Chambers</a>
+ */
 public class ApnsEnvironment {
 	private final String apnsHost;
 	private final int apnsPort;
@@ -9,6 +20,15 @@ public class ApnsEnvironment {
 	
 	private final boolean tlsRequired;
 	
+	/**
+	 * Constructs a new APenvironment with the given host names and ports.
+	 * 
+	 * @param apnsHost the host name of the APNs gateway
+	 * @param apnsPort the TCP port for the APNs gateway
+	 * @param feedbackHost the host name of the APNs feedback service
+	 * @param feedbackPort the TCP port for the APNs feedback service
+	 * @param tlsRequired {@code true} if this environment requires TLS or {@code false} otherwise
+	 */
 	public ApnsEnvironment(final String apnsHost, final int apnsPort, final String feedbackHost, final int feedbackPort, final boolean tlsRequired) {
 		this.apnsHost = apnsHost;
 		this.apnsPort = apnsPort;
@@ -19,30 +39,65 @@ public class ApnsEnvironment {
 		this.tlsRequired = tlsRequired;
 	}
 	
+	/**
+	 * Returns the host name of the APNs gateway in this environment.
+	 * 
+	 * @return the host name of the APNs gateway in this environment
+	 */
 	public String getApnsHost() {
 		return this.apnsHost;
 	}
 	
+	/**
+	 * Returns the TCP port for the APNs gateway in this environment.
+	 * 
+	 * @return the TCP port for the APNs gateway in this environment
+	 */
 	public int getApnsPort() {
 		return this.apnsPort;
 	}
 	
+	/**
+	 * Returns the host name of the APNs feedback service in this environment.
+	 * 
+	 * @return the host name of the APNs feedback service in this environment
+	 */
 	public String getFeedbackHost() {
 		return this.feedbackHost;
 	}
 	
+	/**
+	 * Returns the TCP port for the APNs feedback service in this environment.
+	 * 
+	 * @return the TCP port for the APNs feedback service in this environment
+	 */
 	public int getFeedbackPort() {
 		return this.feedbackPort;
 	}
 	
+	/**
+	 * Indicates whether this environment requires TLS.
+	 * 
+	 * @return {@code true} if this environment requires TLS or {@code false} otherwise
+	 */
 	public boolean isTlsRequired() {
 		return this.tlsRequired;
 	}
 	
+	/**
+	 * Returns an APNs environment for connecting to Apple's production servers.
+	 * 
+	 * @return an APNs environment for connecting to Apple's production servers
+	 */
 	public static ApnsEnvironment getProductionEnvironment() {
 		return new ApnsEnvironment("gateway.push.apple.com", 2195, "feedback.push.apple.com", 2196, true);
 	}
 	
+	/**
+	 * Returns an APNs environment for connecting to Apple's sandbox servers.
+	 * 
+	 * @return an APNs environment for connecting to Apple's sandbox servers
+	 */
 	public static ApnsEnvironment getSandboxEnvironment() {
 		return new ApnsEnvironment("gateway.sandbox.push.apple.com", 2195, "feedback.sandbox.push.apple.com", 2196, true);
 	}
