c773de732b7e91864f5d1286fd45683b66ab7428
Jon Chambers <jon.chambers@gmail.com>
2014-02-07 22:16:18 -0500

    Generate additional keystores for testing various failure conditions.

--- script/generate-keys.sh
+++ script/generate-keys.sh
@@ -15,3 +15,15 @@ keytool -import -storepass pushy-test -keystore pushy-test-server.jks -alias pus
 # Remove imported certificates
 rm pushy-test-server.crt
 rm pushy-test-client.crt
+
+# Generate a client key not trusted by the server
+keytool -genkey -alias pushy-test-client-untrusted -keysize 2048 -validity 36500 -keyalg RSA -dname "CN=pushy" -keypass pushy-test -storepass pushy-test -keystore pushy-test-client-untrusted.jks
+
+# Generate an empty keystore
+keytool -genkey -alias temp -dname "CN=pushy" -keypass pushy-test -storepass pushy-test -keystore empty-keystore.jks
+keytool -delete -alias temp -dname "CN=pushy" -keypass pushy-test -storepass pushy-test -keystore empty-keystore.jks
+
+# Export trusted and untrusted client keys (including the empty keystore) as PKCS12 files
+keytool -importkeystore -alias pushy-test-client -srckeystore pushy-test-client.jks -destkeystore pushy-test-client.p12 -deststoretype PKCS12 -keypass pushy-test -storepass pushy-test -srcstorepass pushy-test
+keytool -importkeystore -alias pushy-test-client-untrusted -srckeystore pushy-test-client-untrusted.jks -destkeystore pushy-test-client-untrusted.p12 -deststoretype PKCS12 -keypass pushy-test -storepass pushy-test -srcstorepass pushy-test
+keytool -importkeystore -srckeystore empty-keystore.jks -destkeystore empty.p12 -deststoretype PKCS12 -keypass pushy-test -storepass pushy-test -srcstorepass pushy-test
