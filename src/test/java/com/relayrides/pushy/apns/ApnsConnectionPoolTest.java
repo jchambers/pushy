44a6a5b667432a59205de0a8c5ae1e0d070e2c75
Jon Chambers <jon@relayrides.com>
2014-02-12 17:33:20 -0500

    Minor: the test should time out if we get interrupted.

--- src/test/java/com/relayrides/pushy/apns/ApnsConnectionPoolTest.java
+++ src/test/java/com/relayrides/pushy/apns/ApnsConnectionPoolTest.java
@@ -110,10 +110,10 @@ public class ApnsConnectionPoolTest {
 			public void run() {
 				try {
 					Thread.sleep(50);
+					pool.removeConnection(testConnection);
 				} catch (InterruptedException ignored) {
 				}
 
-				pool.removeConnection(testConnection);
 			}
 		}).start();
 
