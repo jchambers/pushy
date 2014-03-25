fb8a274a4e75d2a31857c1efd6cea33f52f1e7b3
Jon Chambers <jon.chambers@gmail.com>
2014-03-24 21:48:46 -0400

    No need to call `submit` since we're just discarding the `Future`.

--- src/main/java/com/relayrides/pushy/apns/PushManager.java
+++ src/main/java/com/relayrides/pushy/apns/PushManager.java
@@ -500,7 +500,7 @@ public class PushManager<T extends ApnsPushNotification> implements ApnsConnecti
 		for (final RejectedNotificationListener<? super T> listener : this.rejectedNotificationListeners) {
 
 			// Handle the notifications in a separate thread in case a listener takes a long time to run
-			this.rejectedNotificationExecutorService.submit(new Runnable() {
+			this.rejectedNotificationExecutorService.execute(new Runnable() {
 				public void run() {
 					listener.handleRejectedNotification(rejectedNotification, reason);
 				}
