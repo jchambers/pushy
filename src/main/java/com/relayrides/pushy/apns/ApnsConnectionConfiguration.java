c02398bca51e0d9040fff8df8383a5936f8406f9
Jon Chambers <jon@relayrides.com>
2014-06-06 17:16:10 -0400

    Added preliminary docs for the new configuration objects.

--- src/main/java/com/relayrides/pushy/apns/ApnsConnectionConfiguration.java
+++ src/main/java/com/relayrides/pushy/apns/ApnsConnectionConfiguration.java
@@ -1,21 +1,46 @@
 package com.relayrides.pushy.apns;
 
+/**
+ * A set of user-configurable options that affect the behavior of an {@link ApnsConnection}.
+ *
+ * @author <a href="mailto:jon@relayrides.com">Jon Chambers</a>
+ */
 public class ApnsConnectionConfiguration {
 
 	private int sentNotificationBufferCapacity = ApnsConnection.DEFAULT_SENT_NOTIFICATION_BUFFER_CAPACITY;
 
+	/**
+	 * Creates a new connection configuration object with all options set to their default values.
+	 */
 	public ApnsConnectionConfiguration() {}
 
+	/**
+	 * Creates a new connection configuration object with all options set to the values in the given connection
+	 * configuration object.
+	 *
+	 * @param configuration the configuration object to copy
+	 */
 	public ApnsConnectionConfiguration(final ApnsConnectionConfiguration configuration) {
 		this.sentNotificationBufferCapacity = configuration.sentNotificationBufferCapacity;
 	}
 
+	/**
+	 * Returns the sent notification buffer capacity for connections created with this configuration.
+	 *
+	 * @return the sent notification buffer capacity for connections created with this configuration
+	 */
 	public int getSentNotificationBufferCapacity() {
 		return sentNotificationBufferCapacity;
 	}
 
-	public void setSentNotificationBufferCapacity(
-			int sentNotificationBufferCapacity) {
+	/**
+	 * Sets the sent notification buffer capacity for connections created with this configuration. The default capacity
+	 * is {@value ApnsConnection#DEFAULT_SENT_NOTIFICATION_BUFFER_CAPACITY} notifications.
+	 *
+	 * @param sentNotificationBufferCapacity the sent notification buffer capacity for connections created with this
+	 * configuration
+	 */
+	public void setSentNotificationBufferCapacity(int sentNotificationBufferCapacity) {
 		this.sentNotificationBufferCapacity = sentNotificationBufferCapacity;
 	}
 }
--- src/main/java/com/relayrides/pushy/apns/PushManagerConfiguration.java
+++ src/main/java/com/relayrides/pushy/apns/PushManagerConfiguration.java
@@ -1,7 +1,8 @@
 package com.relayrides.pushy.apns;
 
 /**
- *
+ * A set of user-configurable options that affect the behavior of a {@link PushManager} and its associated
+ * {@link ApnsConnection}s.
  *
  * @author <a href="mailto:jon@relayrides.com">Jon Chambers</a>
  */
@@ -9,23 +10,67 @@ public class PushManagerConfiguration {
 
 	private int concurrentConnectionCount = 1;
 
-	private final ApnsConnectionConfiguration connectionConfiguration = new ApnsConnectionConfiguration();
+	private ApnsConnectionConfiguration connectionConfiguration = new ApnsConnectionConfiguration();
 
+	/**
+	 * Constructs a new push manager configuration object with all options set to their default values.
+	 */
 	public PushManagerConfiguration() {}
 
+	/**
+	 * Constructs a new push manager configuration object with all options set to the values in the given configuration
+	 * object.
+	 *
+	 * @param configuration the configuration object to copy
+	 */
 	public PushManagerConfiguration(final PushManagerConfiguration configuration) {
 		this.concurrentConnectionCount = configuration.getConcurrentConnectionCount();
+		this.connectionConfiguration = new ApnsConnectionConfiguration(configuration.getConnectionConfiguration());
 	}
 
+	/**
+	 * Returns the number of concurrent connections to be maintained by push managers created with this configuration.
+	 *
+	 * @return the number of concurrent connections to be maintained by push managers created with this configuration
+	 */
 	public int getConcurrentConnectionCount() {
 		return concurrentConnectionCount;
 	}
 
+	/**
+	 * Sets the number of concurrent connections to be maintained by push managers created with this configuration. By
+	 * default, push managers will maintain a single connection to the APNs gateway.
+	 *
+	 * @param concurrentConnectionCount the number of concurrent connections to be maintained by push managers created
+	 * with this configuration
+	 */
 	public void setConcurrentConnectionCount(int concurrentConnectionCount) {
 		this.concurrentConnectionCount = concurrentConnectionCount;
 	}
 
+	/**
+	 * Returns the configuration to be used for connections created by push managers created with this configuration. A
+	 * set of default connection options is used if none is specified.
+	 *
+	 * @return the configuration to be used for connections created by push managers created with this configuration
+	 */
 	public ApnsConnectionConfiguration getConnectionConfiguration() {
 		return connectionConfiguration;
+	}
+
+	/**
+	 * Sets the configuration to be used for connections created by push managers created with this configuration.
+	 *
+	 * @param connectionConfiguration the configuration to be used for connections created by push managers created with
+	 * this configuration; must not be {@code null}
+	 *
+	 * @throws NullPointerException if the given connection configuration is {@code null}
+	 */
+	public void setConnectionConfiguration(final ApnsConnectionConfiguration connectionConfiguration) {
+		if (connectionConfiguration == null) {
+			throw new NullPointerException("Connection configuration must not be null.");
+		}
+
+		this.connectionConfiguration = connectionConfiguration;
 	}
 }
