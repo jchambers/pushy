ef6763df1dd33f63cfe9c27c6426b6d89cfb3541
Jon Chambers <jon@relayrides.com>
2014-07-13 14:17:03 -0400

    Added a `closeAfterInactivityTime` option to channel config objects. Doesn't have teeth yet.

--- src/main/java/com/relayrides/pushy/apns/ApnsConnectionConfiguration.java
+++ src/main/java/com/relayrides/pushy/apns/ApnsConnectionConfiguration.java
@@ -8,6 +8,7 @@ package com.relayrides.pushy.apns;
 public class ApnsConnectionConfiguration {
 
 	private int sentNotificationBufferCapacity = ApnsConnection.DEFAULT_SENT_NOTIFICATION_BUFFER_CAPACITY;
+	private Integer closeAfterInactivityTime = null;
 
 	/**
 	 * Creates a new connection configuration object with all options set to their default values.
@@ -42,27 +43,60 @@ public class ApnsConnectionConfiguration {
 	 * @param sentNotificationBufferCapacity the sent notification buffer capacity for connections created with this
 	 * configuration
 	 */
-	public void setSentNotificationBufferCapacity(int sentNotificationBufferCapacity) {
+	public void setSentNotificationBufferCapacity(final int sentNotificationBufferCapacity) {
 		this.sentNotificationBufferCapacity = sentNotificationBufferCapacity;
+	}
+
+	/**
+	 * Returns the time, in seconds, since the last push notification was sent after which connections created with this
+	 * configuration will be closed. If {@code null}, connections created with this configuration will never be closed
+	 * due to inactivity.
+	 *
+	 * @return the time, in seconds, since the last push notification was sent after which connections created with this
+	 * configuration will be closed
+	 */
+	public Integer getCloseAfterInactivityTime() {
+		return this.closeAfterInactivityTime;
+	}
+
+	/**
+	 * Sets the time, in seconds, since the last push notification was sent after which connections created with this
+	 * configuration will be closed. If {@code null} (the default), connections will never be closed due to inactivity.
+	 *
+	 * @param closeAfterInactivityTime the time, in seconds since the last push notification was sent, after which
+	 * connections will be closed
+	 */
+	public void setCloseAfterInactivityTime(final Integer closeAfterInactivityTime) {
+		this.closeAfterInactivityTime = closeAfterInactivityTime;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime
+				* result
+				+ ((closeAfterInactivityTime == null) ? 0
+						: closeAfterInactivityTime.hashCode());
 		result = prime * result + sentNotificationBufferCapacity;
 		return result;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(final Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		ApnsConnectionConfiguration other = (ApnsConnectionConfiguration) obj;
+		final ApnsConnectionConfiguration other = (ApnsConnectionConfiguration) obj;
+		if (closeAfterInactivityTime == null) {
+			if (other.closeAfterInactivityTime != null)
+				return false;
+		} else if (!closeAfterInactivityTime
+				.equals(other.closeAfterInactivityTime))
+			return false;
 		if (sentNotificationBufferCapacity != other.sentNotificationBufferCapacity)
 			return false;
 		return true;
