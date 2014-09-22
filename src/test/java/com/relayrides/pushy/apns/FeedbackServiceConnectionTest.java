efe99b4f6a384e9cf5068d99c5273c2d86569fc1
Jon Chambers <jon@relayrides.com>
2014-09-22 17:30:04 -0400

    Restored FeedbackServiceConnectionTest.

--- src/test/java/com/relayrides/pushy/apns/FeedbackServiceConnectionTest.java
+++ src/test/java/com/relayrides/pushy/apns/FeedbackServiceConnectionTest.java
@@ -21,16 +21,78 @@
 
 package com.relayrides.pushy.apns;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
 
 public class FeedbackServiceConnectionTest extends BasePushyTest {
 
-	/* @Test
-	public void testGetExpiredTokens() throws Exception {
-		final FeedbackServiceConnection feedbackClient =
-				new FeedbackServiceConnection(TEST_ENVIRONMENT, SSLTestUtil.createSSLContextForTestClient(),
-						this.getEventLoopGroup(), new FeedbackConnectionConfiguration());
+	private class TestListener implements FeedbackServiceListener {
 
-		assertTrue(feedbackClient.getExpiredTokens().isEmpty());
+		private final Object mutex;
+
+		private boolean connectionSucceeded = false;
+		private boolean connectionFailed = false;
+		private boolean connectionClosed = false;
+
+		private Throwable connectionFailureCause;
+
+		private final ArrayList<ExpiredToken> expiredTokens = new ArrayList<ExpiredToken>();
+
+		public TestListener(final Object mutex) {
+			this.mutex = mutex;
+		}
+
+		@Override
+		public void handleConnectionSuccess(final FeedbackServiceConnection connection) {
+			synchronized (this.mutex) {
+				this.connectionSucceeded = true;
+				this.mutex.notifyAll();
+			}
+		}
+
+		@Override
+		public void handleConnectionFailure(final FeedbackServiceConnection connection, final Throwable cause) {
+			synchronized (this.mutex) {
+				this.connectionFailed = true;
+				this.connectionFailureCause = cause;
+
+				this.mutex.notifyAll();
+			}
+		}
+
+		@Override
+		public void handleExpiredToken(final FeedbackServiceConnection connection, final ExpiredToken token) {
+			this.expiredTokens.add(token);
+		}
+
+		@Override
+		public void handleConnectionClosure(final FeedbackServiceConnection connection) {
+			synchronized (this.mutex) {
+				this.connectionClosed = true;
+				this.mutex.notifyAll();
+			}
+		}
+	}
+
+	private static final String TEST_CONNETION_NAME = "TestFeedbackConnection";
+
+	@Test
+	public void testGetExpiredTokens() throws Exception {
+		final Object mutex = new Object();
+
+		final TestListener listener = new TestListener(mutex);
+
+		final FeedbackServiceConnection feedbackConnection =
+				new FeedbackServiceConnection(TEST_ENVIRONMENT, SSLTestUtil.createSSLContextForTestClient(),
+						this.getEventLoopGroup(), new FeedbackConnectionConfiguration(), listener, TEST_CONNETION_NAME);
+
+		assertTrue(listener.expiredTokens.isEmpty());
 
 		// Dates will have some loss of precision since APNS only deals with SECONDS since the epoch; we choose
 		// timestamps that just happen to be on full seconds.
@@ -40,18 +102,30 @@ public class FeedbackServiceConnectionTest extends BasePushyTest {
 		this.getFeedbackServer().addExpiredToken(firstToken);
 		this.getFeedbackServer().addExpiredToken(secondToken);
 
-		final List<ExpiredToken> expiredTokens = feedbackClient.getExpiredTokens();
+		synchronized (mutex) {
+			feedbackConnection.connect();
 
-		assertEquals(2, expiredTokens.size());
-		assertTrue(expiredTokens.contains(firstToken));
-		assertTrue(expiredTokens.contains(secondToken));
+			while (!listener.connectionSucceeded) {
+				mutex.wait();
+			}
+		}
 
-		assertTrue(feedbackClient.getExpiredTokens().isEmpty());
+		assertTrue(listener.connectionSucceeded);
+
+		synchronized (mutex) {
+			while (!listener.connectionClosed) {
+				mutex.wait();
+			}
+		}
+
+		assertEquals(2, listener.expiredTokens.size());
+		assertTrue(listener.expiredTokens.contains(firstToken));
+		assertTrue(listener.expiredTokens.contains(secondToken));
 	}
 
 	@Test
 	public void testGetExpiredTokensCloseWhenDone() throws Exception {
 		this.getFeedbackServer().setCloseWhenDone(true);
 		this.testGetExpiredTokens();
-	} */
+	}
 }
