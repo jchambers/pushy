40eee3e3d27d94cd0653e37679f490bafbb606fd
Jon Chambers <jon.chambers@gmail.com>
2015-12-26 18:07:37 -0500

    Added an affirmative test for sending with an explicit topic.

--- src/test/java/com/relayrides/pushy/apns/ApnsClientTest.java
+++ src/test/java/com/relayrides/pushy/apns/ApnsClientTest.java
@@ -203,6 +203,29 @@ public class ApnsClientTest {
     }
 
     @Test
+    public void testSendNotificationWithSpecifiedTopic() throws Exception {
+        final ApnsClient<SimpleApnsPushNotification> multiTopicClient = new ApnsClient<>(
+                ApnsClientTest.getSslContextForTestClient(MULTI_TOPIC_CLIENT_KEYSTORE_FILENAME, CLIENT_KEYSTORE_PASSWORD),
+                EVENT_LOOP_GROUP);
+
+        multiTopicClient.connect("localhost", 8443).get();
+
+        final String testToken = ApnsClientTest.generateRandomToken();
+
+        this.server.registerToken(DEFAULT_TOPIC, testToken);
+
+        final SimpleApnsPushNotification pushNotification =
+                new SimpleApnsPushNotification(testToken, "test-payload", null, DeliveryPriority.IMMEDIATE, DEFAULT_TOPIC);
+
+        final PushNotificationResponse<SimpleApnsPushNotification> response =
+                multiTopicClient.sendNotification(pushNotification).get();
+
+        multiTopicClient.disconnect().get();
+
+        assertTrue(response.isSuccess());
+    }
+
+    @Test
     public void testSendNotificationWithUnregisteredToken() throws Exception {
         final SimpleApnsPushNotification pushNotification =
                 new SimpleApnsPushNotification(ApnsClientTest.generateRandomToken(), "test-payload");
