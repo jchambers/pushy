67bba152190df2f45475a2f5cf038fd1ee365525
Jon Chambers <jon.chambers@gmail.com>
2015-12-30 19:30:52 -0500

    Renamed some arguments for clarity; restored SslContextUtil docs.

--- src/main/java/com/relayrides/pushy/apns/util/SslContextUtil.java
+++ src/main/java/com/relayrides/pushy/apns/util/SslContextUtil.java
@@ -24,12 +24,41 @@ import io.netty.handler.ssl.ApplicationProtocolConfig.SelectorFailureBehavior;
  */
 public class SslContextUtil {
 
-    public static SslContext createDefaultSslContext(final File certificateFile, final File privateKeyFile, final String privateKeyPassword) throws SSLException {
+    /**
+     * Creates a new SSL context for an {@link ApnsClient} with a certificate and key from the given files. The
+     * certificate file <em>must</em> contain a PEM-formatted X.509 certificate, and the key file <em>must</em> contain
+     * a PKCS8-formatted private key.
+     *
+     * @param certificatePemFile a PEM-formatted file containing an X.509 certificate to be used to identify the client
+     * to the APNs server
+     * @param privateKeyPkcs8File a PKCS8-formatted file containing a private key for the client certificate
+     * @param privateKeyPassword the password to be used to decrypt the private key; may be {@code null} if the private
+     * key does not require a password
+     *
+     * @return an SSL context with the given client credentials suitable for use with an {@link ApnsClient}
+     *
+     * @throws SSLException if the given key or certificate could not be loaded or if any other SSL-related problem
+     * arises when constructing the context
+     */
+    public static SslContext createDefaultSslContext(final File certificatePemFile, final File privateKeyPkcs8File, final String privateKeyPassword) throws SSLException {
         return getDefaultBuilder()
-                .keyManager(certificateFile, privateKeyFile, privateKeyPassword)
+                .keyManager(certificatePemFile, privateKeyPkcs8File, privateKeyPassword)
                 .build();
     }
 
+    /**
+     * Creates a new SSL context for an {@link ApnsClient} with the given certificate private key.
+     *
+     * @param certificate the certificate to be used to identify the client to the APNs server
+     * @param privateKey the private key for the client certificate
+     * @param privateKeyPassword the password to be used to decrypt the private key; may be {@code null} if the private
+     * key does not require a password
+     *
+     * @return an SSL context with the given client credentials suitable for use with an {@link ApnsClient}
+     *
+     * @throws SSLException if the given key or certificate could not be loaded or if any other SSL-related problem
+     * arises when constructing the context
+     */
     public static SslContext createDefaultSslContext(final X509Certificate certificate, final PrivateKey privateKey, final String privateKeyPassword) throws SSLException {
         return getDefaultBuilder()
                 .keyManager(privateKey, privateKeyPassword, certificate)
