bc503b99fda6162906ffd6293bfd6e9123cba514
Jon Chambers <jon@relayrides.com>
2016-01-10 21:28:31 -0500

    Fixed a dumb typo.

--- src/main/java/com/relayrides/pushy/apns/util/package-info.java
+++ src/main/java/com/relayrides/pushy/apns/util/package-info.java
@@ -29,8 +29,8 @@
  * <p>Device tokens identify the device to which a push notification is being sent. Ultimately, tokens need to be
  * expressed as a string of hexadecimal characters, but a common practice is to transmit tokens as the output of
  * <a href="https://developer.apple.com/library/mac/documentation/Cocoa/Reference/Foundation/Classes/NSData_Class/Reference/Reference.html#//apple_ref/occ/instm/NSData/description">
- * {@code [NSData describe]}</a>. The {@link com.relayrides.pushy.apns.util.TokenUtil} class provides sanitizing token
- * strings so they can be sent safely to the APNs gateway.</p>
+ * {@code [NSData describe]}</a>. The {@link com.relayrides.pushy.apns.util.TokenUtil} class provides methods for
+ * sanitizing token strings so they can be sent safely to the APNs gateway.</p>
  *
  * @author <a href="mailto:jon@relayrides.com">Jon Chambers</a>
  */
