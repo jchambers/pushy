835b1a8c53207c9a7c7dee2a4149a2676e44fc83
Justin Mason <justin.mason@swrve.com>
2016-01-25 16:16:40 +0000

    Add note re metric-set prefix

--- src/main/java/com/relayrides/pushy/apns/metrics/PushyMetrics.java
+++ src/main/java/com/relayrides/pushy/apns/metrics/PushyMetrics.java
@@ -5,6 +5,12 @@ package com.relayrides.pushy.apns.metrics;
  *
  * This is pretty simplistic; improvements could include enum keys instead of strings for the metric
  * names, or pre-declared metric objects encapsulating the state for a Counter, a Timer, etc.
+ *
+ * I would envisage a set of additional Maven modules for "pushy-dropwizard-metrics-publisher",
+ * "pushy-statsd-metrics-publisher" etc., which provide the necessary dependency declarations,
+ * and a factory to create PushyMetrics objects suitable to publish to whatever metrics system
+ * is in use.  (Regarding https://github.com/relayrides/pushy/issues/175#issuecomment-172857510 ,
+ * this would be the ideal place to specify the string prefix for the ApnsClient's metrics set.)
  */
 public interface PushyMetrics {
 
