a00d23395ccf38a153959f3dce11b41274ef0ae0
Jon Chambers <jon.chambers@gmail.com>
2016-03-12 12:16:07 -0500

    Added docs for the ApnsClientMetricsListener interface.

--- src/main/java/com/relayrides/pushy/apns/metrics/ApnsClientMetricsListener.java
+++ src/main/java/com/relayrides/pushy/apns/metrics/ApnsClientMetricsListener.java
@@ -36,12 +36,66 @@ import com.relayrides.pushy.apns.ApnsPushNotification;
  * @author <a href="https://github.com/jchambers">Jon Chambers</a>
  */
 public interface ApnsClientMetricsListener {
+
+    /**
+     * Indicates that an attempt to send a push notification failed before the notification was processed by the APNs
+     * server. Write failures may be the first event in a sequence for a given notification ID (indicating that the
+     * notification was never written to the wire), but may also occur after a notification was sent if the connection
+     * closed before the notification was acknowledged by the serer.
+     *
+     * @param apnsClient the client that sent the notification
+     * @param notificationId an opaque identifier for the push notification that can be used to correlate this event
+     * with other events related to the same notification
+     */
     void handleWriteFailure(ApnsClient<? extends ApnsPushNotification> apnsClient, long notificationId);
+
+    /**
+     * Indicates that a notification was sent to the APNs server.
+     *
+     * @param apnsClient the client that sent the notification
+     * @param notificationId an opaque identifier for the push notification that can be used to correlate this event
+     * with other events related to the same notification
+     */
     void handleNotificationSent(ApnsClient<? extends ApnsPushNotification> apnsClient, long notificationId);
+
+    /**
+     * Indicates that a notification that was previously sent to an APNs server was accepted by the server.
+     *
+     * @param apnsClient the client that sent the notification
+     * @param notificationId an opaque identifier for the push notification that can be used to correlate this event
+     * with other events related to the same notification
+     */
     void handleNotificationAccepted(ApnsClient<? extends ApnsPushNotification> apnsClient, long notificationId);
+
+    /**
+     * Indicates that a notification that was previously sent to an APNs server was rejected by the server.
+     *
+     * @param apnsClient the client that sent the notification
+     * @param notificationId an opaque identifier for the push notification that can be used to correlate this event
+     * with other events related to the same notification
+     */
     void handleNotificationRejected(ApnsClient<? extends ApnsPushNotification> apnsClient, long notificationId);
 
+    /**
+     * Indicates that the client has started an attempt to connect to an APNs server. This event will always be followed
+     * by either a "connection attempt succeeded" or a "connection attempt failed" event; no other "connection attempt
+     * started" events will be fired until the current attempt has resolved.
+     *
+     * @param apnsClient the client that started the connection attempt
+     */
     void handleConnectionAttemptStarted(ApnsClient<? extends ApnsPushNotification> apnsClient);
+
+    /**
+     * Indicates that a previously-started connection attempt completed successfully.
+     *
+     * @param apnsClient the client that started the connection attempt
+     */
     void handleConnectionAttemptSucceeded(ApnsClient<? extends ApnsPushNotification> apnsClient);
+
+    /**
+     * Indicates that a previously-started connection attempt failed.
+     *
+     * @param apnsClient the client that started the connection attempt
+     */
     void handleConnectionAttemptFailed(ApnsClient<? extends ApnsPushNotification> apnsClient);
 }
