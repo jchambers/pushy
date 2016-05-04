4f19ae032e71eb13fab3efe967d402e1141b9cc3
Jon Chambers <jon@turo.com>
2016-05-04 12:25:50 -0400

    Made payload builder benchmarks a bit more meaningful.

--- benchmark/src/main/java/com/relayrides/pushy/apns/util/ApnsPayloadBuilderBenchmark.java
+++ benchmark/src/main/java/com/relayrides/pushy/apns/util/ApnsPayloadBuilderBenchmark.java
@@ -1,5 +1,6 @@
 package com.relayrides.pushy.apns.util;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
@@ -12,16 +13,26 @@ public class ApnsPayloadBuilderBenchmark {
 
     private static final int MAXIMUM_PAYLOAD_SIZE = 1024 * 8;
 
-    private static final String SHORT_MESSAGE_BODY = "This is a short message body.";
+    private String shortAsciiMessageBody;
+    private String longAsciiMessageBody;
 
     @Setup
     public void setUp() {
         this.apnsPayloadBuilder = new ApnsPayloadBuilder();
+
+        this.shortAsciiMessageBody = RandomStringUtils.randomAscii(MAXIMUM_PAYLOAD_SIZE / 8);
+        this.longAsciiMessageBody = RandomStringUtils.randomAscii(MAXIMUM_PAYLOAD_SIZE * 2);
     }
 
     @Benchmark
-    public String testShortUnabbreviatedMessageBody() {
-        this.apnsPayloadBuilder.setAlertBody(SHORT_MESSAGE_BODY);
+    public String testShortAsciiMessageBody() {
+        this.apnsPayloadBuilder.setAlertBody(this.shortAsciiMessageBody);
+        return this.apnsPayloadBuilder.buildWithMaximumLength(MAXIMUM_PAYLOAD_SIZE);
+    }
+
+    @Benchmark
+    public String testLongAsciiMessageBody() {
+        this.apnsPayloadBuilder.setAlertBody(this.longAsciiMessageBody);
         return this.apnsPayloadBuilder.buildWithMaximumLength(MAXIMUM_PAYLOAD_SIZE);
     }
 }
