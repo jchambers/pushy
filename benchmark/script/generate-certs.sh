d484f592b7960c715f62977ea4638a21c4888915
Jon Chambers <jon.chambers@gmail.com>
2016-06-26 14:57:11 -0400

    Updated the client benchmark to use auth tokens.

--- benchmark/script/generate-certs.sh
+++ benchmark/script/generate-certs.sh
@@ -7,13 +7,6 @@ openssl req -extensions v3_ca -new -x509 -days 36500 -nodes -subj "/CN=PushyTest
 openssl req -new -keyout server_key.pem -nodes -newkey rsa:2048 -subj "/CN=com.relayrides.pushy" | \
     openssl x509 -req -CAkey ca.key -CA ca.pem -days 36500 -set_serial $RANDOM -sha512 -out server_certs.pem
 
-# Generate certificates/keys for clients and sign them with the intermediate certificate
-openssl req -new -keyout client.key -nodes -newkey rsa:2048 -subj "/CN=Apple Push Services: com.relayrides.pushy/UID=com.relayrides.pushy" | \
-    openssl x509 -req -CAkey ca.key -CA ca.pem -days 36500 -set_serial $RANDOM -sha512 -out client.pem
-
-# For convenience, squish the client credentials down into a PKCS#12 keystore
-openssl pkcs12 -export -in client.pem -inkey client.key -out client.p12 -password pass:pushy-test
-
 # Clean up intermediate files
 rm ca.key
 rm client.key
