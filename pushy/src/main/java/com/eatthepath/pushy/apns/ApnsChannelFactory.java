fc30c9e8e1c3d5856975b0be6320a42aa9d51608
udayanmajumdar <udayanmajumdar@fico.com>
2020-10-12 14:11:04 -0700

    SNI Support for push notifications

--- pushy/src/main/java/com/eatthepath/pushy/apns/ApnsChannelFactory.java
+++ pushy/src/main/java/com/eatthepath/pushy/apns/ApnsChannelFactory.java
@@ -101,12 +101,14 @@ class ApnsChannelFactory implements PooledObjectFactory<Channel>, Closeable {
 
             @Override
             protected void initChannel(final SocketChannel channel) {
-                final SslHandler sslHandler = sslContext.newHandler(channel.alloc());
+            	 
+            	final String authority = apnsServerAddress.getHostName();
+
+                final SslHandler sslHandler = sslContext.newHandler(channel.alloc(),authority,443);
 
                 final ApnsClientHandler apnsClientHandler;
                 {
-                    final String authority = apnsServerAddress.getHostName();
-
+                   
                     final ApnsClientHandler.ApnsClientHandlerBuilder clientHandlerBuilder;
 
                     if (signingKey != null) {
