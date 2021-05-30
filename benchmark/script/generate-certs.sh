a0e03636bf0f1b7c5ad2b04cb76dda147ce82d67
Jon Chambers <jon.chambers@gmail.com>
2021-05-30 13:06:36 -0400

    Specify a key algorithm and don't rely on bash-specific features when generating test keys/certificates.

--- benchmark/script/generate-certs.sh
+++ benchmark/script/generate-certs.sh
@@ -5,7 +5,7 @@ openssl req -extensions v3_ca -new -x509 -days 36500 -nodes -subj "/CN=PushyTest
 
 # Generate a certificate/key for the server
 openssl req -new -keyout server_key.pem -nodes -newkey rsa:2048 -subj "/CN=com.eatthepath.pushy" | \
-    openssl x509 -req -CAkey ca.key -CA ca.pem -days 36500 -set_serial $RANDOM -sha512 -out server_certs.pem
+    openssl x509 -req -CAkey ca.key -CA ca.pem -days 36500 -set_serial 1 -sha512 -out server_certs.pem
 
 # Clean up intermediate files
 rm ca.key
